4d21bfcce5d352a5d174949cc311bf81af2cf8fc
Beasleydog <beasleypero@gmail.com>
2022-02-07 18:21:32 -0500

    begin working on homepage and setup tailwind styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,14 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className="bg-blue-200 w-4 h-5 text-xs" >
-        test
+      <div className="bg-blue font-bold w-full h-[100px] flex items-center" >
+        <div className='flex items-center justify-center w-[50vw]'>
+          <div className="font-main text-white text-[40px]">
+            Debuggers
+          </div>
+        </div>
+        <div className='flex items-center justify-center w-[50vw]'>
+        </div>
       </div >
     );
   }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'main': ['"Share Tech Mono"', 'cursive']
+      }
+    },
+    colors: {
+      blue: "#03213A",
+      white: "#F9F8F8",
+      green: "#9BC53D",
+      red: "#F71735",
+      black: "#373F47",
+      yellow: "#F2F03F"
+    }
   },
   plugins: [],
 }
