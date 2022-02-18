6e7a48365ce3bbf8fa7fde4480c8d6033030e7e2
Beasleydog <beasleypero@gmail.com>
2022-02-17 20:05:39 -0500

    basic design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
       <div className="fixed top-0 left-0 backdrop-blur shadow-lg font-bold w-full h-[80px] flex items-center z-50">
         <div className="flex items-center -left-px p-6 w-[50vw]">
           <div className="font-main text-white text-[30px]">Debuggers</div>
-          <button className="absolute right-12 font-main text-white text-[20px] hover:underline">
+          <button className="absolute right-12 text-white text-[20px] hover:underline">
             Sign In
           </button>
           <button className="absolute right-40 no-underline font-main text-white text-[20px] hover:underline">
@@ -27,15 +27,10 @@ function Home() {
           content={
             <div className="flex items-center justify-center w-full h-full">
               <div className="flex flex-col gap-6 items-center justify-center">
-                <div className="break-normal text-white text-[40px] text-center">
+                <div className="break-normal text-white text-[40px] text-center font-bold">
                   The Free, Fun, And Effective Way To Learn Web Design
                 </div>
-                <button
-                  type="button"
-                  className="text-white text-[25px] rounded-[10px] font-bold py-3 px-6 rounded-boxed border-white border-2 w-max"
-                >
-                  Get started
-                </button>
+                <GetStartedButton />
               </div>
             </div>
           }
@@ -43,23 +38,62 @@ function Home() {
         <Section
           glows={[{ x: "0", y: "0" }]}
           content={
-            <div className="w-full h-full font-bold flex justify-center items-center">
-              <div className="flex flex-col gap-6 items-center">
-                <div className="break-normal text-white text-[40px] text-center">
-                  The Free, Fun, And Effective Way To Learn Web Design
-                </div>
-                <button
-                  type="button"
-                  className="text-white text-[25px] rounded-[10px] font-bold py-3 px-6 rounded-boxed border-white border-2 w-max"
-                >
-                  Get started
-                </button>
+            <div className="w-full h-full flex flex-col justify-center items-center text-white pt-5">
+              <div className="text-[50px] font-bold">
+                Learn
               </div>
+              <div className="text-[30px]">
+                Learn simple HTML and CSS concepts.
+              </div>
+              <img src="/images/learnDisplay.jpg" className="bg-white rounded w-[50%] h-auto mt-5" />
             </div>
           }
         />
+        <Section
+          glows={[{ x: "0", y: "0" }]}
+          content={
+            <div className="w-full h-full flex flex-col justify-center items-center text-white pt-5">
+              <div className="text-[50px] font-bold">
+                Review
+              </div>
+              <div className="text-[30px]">
+                Review concepts with multiple choice and open ended questions.
+              </div>
+              <img src="/images/reviewDisplay.jpg" className="bg-white rounded w-[50%] h-auto mt-5" />
+            </div>
+          }
+        />
+        <Section
+          glows={[{ x: "0", y: "0" }]}
+          content={
+            <div className="w-full h-full flex flex-col justify-center items-center text-white pt-5">
+              <div className="text-[50px] font-bold">
+                Apply
+              </div>
+              <div className="text-[30px]">
+                Complete design challenges to check for understanding.
+              </div>
+              <img src="/images/applyDisplay.jpg" className="bg-white rounded w-[50%] h-auto mt-5" />
+            </div>
+          }
+        />
+        <Section
+          glows={[{ x: "0", y: "0" }]}
+          content={
+            <div className="w-full h-full flex flex-col justify-center items-center text-white pt-5">
+              <div className="text-[50px] font-bold">
+                Earn
+              </div>
+              <div className="text-[30px]">
+                Earn stars and unlock cosmetics.
+              </div>
+              <img src="/images/applyDisplay.jpg" className="bg-white rounded w-[50%] h-auto mt-5" />
+            </div>
+          }
+        />
+
       </div>
-    </div>
+    </div >
   );
 }
 function Section(props) {
@@ -89,5 +123,13 @@ function Section(props) {
       </div>
     </div>
   );
+}
+function GetStartedButton(props) {
+  return (<button
+    type="button"
+    className="text-white text-[25px] rounded-[10px] font-bold py-3 px-6 rounded-boxed border-white border-2 w-max"
+  >
+    Get started
+  </button>)
 }
 export default Home;
