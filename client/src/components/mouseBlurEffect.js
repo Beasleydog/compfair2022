59236e70fa0e39ee882b9cb62e98cc4c9923a972
Beasleydog <beasleypero@gmail.com>
2022-02-25 18:37:35 -0500

    introduce components folder and move cursor blur to new folder from homepage

---
+++ client/src/components/mouseBlurEffect.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const MouseBlurEffect = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        addEventListeners();
+        return () => removeEventListeners();
+    }, []);
+
+    const addEventListeners = () => {
+        document.addEventListener("mousemove", onMouseMove);
+    };
+
+    const removeEventListeners = () => {
+        document.removeEventListener("mousemove", onMouseMove);
+    };
+
+    const onMouseMove = (e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    return <div className="w-[200px] h-[200px] fixed blur scale-[6] bg-center bg-no-repeat bg-[length:75%_75%]"
+        style={
+            {
+                backgroundImage: `radial-gradient(circle, rgba(64,164,244,0.2786064767703957) 0%, rgba(1,11,19,0) 70%)`,
+                left: `${position.x - 95}px`,
+                top: `${position.y - 95}px`,
+                transition: "left .2s, top .2s"
+            }
+        } />
+}
+export default MouseBlurEffect;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import MouseBlurEffect from "../components/mouseBlurEffect.js"
 function Home() {
   return (
     <div className="font-main bg-black w-full overflow-hidden">
@@ -94,7 +94,7 @@ function Home() {
 
       </div>
 
-      <CursorBlur />
+      <MouseBlurEffect />
     </div >
   );
 }
@@ -134,34 +134,5 @@ function GetStartedButton(props) {
     Get started
   </button>)
 }
-const CursorBlur = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    addEventListeners();
-    return () => removeEventListeners();
-  }, []);
-
-  const addEventListeners = () => {
-    document.addEventListener("mousemove", onMouseMove);
-  };
-
-  const removeEventListeners = () => {
-    document.removeEventListener("mousemove", onMouseMove);
-  };
-
-  const onMouseMove = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-  };
-
-  return <div className="w-[200px] h-[200px] fixed blur scale-[6] bg-center bg-no-repeat bg-[length:75%_75%]"
-    style={
-      {
-        backgroundImage: `radial-gradient(circle, rgba(64,164,244,0.2786064767703957) 0%, rgba(1,11,19,0) 70%)`,
-        left: `${position.x - 95}px`,
-        top: `${position.y - 95}px`,
-        transition: "left .2s, top .2s"
-      }
-    } />
-}
 export default Home;
