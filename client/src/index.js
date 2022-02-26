15a04106fd2fb9f4ad88667d32a986fd2b0076ef
Beasleydog <beasleypero@gmail.com>
2022-02-26 16:06:52 -0500

    setup basic play page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import List from './pages/List';
+import Play from './pages/Play';
 import './styles/output.css';
 
 render(
@@ -10,6 +11,7 @@ render(
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="list" element={<List />} />
+            <Route path="play/:id/:mode" element={<Play />} />
         </Routes>
     </BrowserRouter>
     , document.getElementById('root'));
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MouseBlurEffect from "../components/mouseBlurEffect.js"
+import Button from "../components/button.js"
+import BlurBackground from "../components/blurBackground.js"
 function Home() {
   return (
     <div className="font-main bg-black w-full overflow-hidden">
@@ -30,7 +32,7 @@ function Home() {
                 <div className="break-normal text-white text-[40px] text-center font-bold">
                   The Free, Fun, And Effective Way To Learn Web Design
                 </div>
-                <GetStartedButton />
+                <Button text="Get started" />
               </div>
             </div>
           }
@@ -105,34 +107,9 @@ function Section(props) {
   return (
     <div className={classString}>
       <div className="z-10 relative left-0 top-0 h-full w-full">{props.content}</div>
-      <div className="blur-2xl top-[-100%] relative w-full h-full">
-        {props.glows
-          ? props.glows.map((glow, i) => (
-            <div
-              key={i}
-              className="w-[500px] h-[500px] absolute"
-              style={{
-                backgroundImage: `radial-gradient(circle, rgba(64,164,244,0.6786064767703957) 0%, rgba(1,11,19,0) 70%)`,
-                transform: `scale(3) translate(${glow.x ? glow.x : "0px"},${glow.y ? glow.y : "0px"
-                  })`,
-                backgroundSize: "75% 75%",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat"
-              }}
-            ></div>
-          ))
-          : ""}
-      </div>
+      <BlurBackground glows={props.glows} />
     </div>
   );
-}
-function GetStartedButton(props) {
-  return (<button
-    type="button"
-    className="text-white text-[25px] rounded-[10px] font-bold py-3 px-6 rounded-boxed border-white border-2 w-max"
-  >
-    Get started
-  </button>)
 }
 
 export default Home;
