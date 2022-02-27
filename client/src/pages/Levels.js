31312ee0a8c6965a5bb45e1c4eecb62208f39f7f
Beasleydog <beasleypero@gmail.com>
2022-02-26 22:40:12 -0500

    de jankify

--- client/src/pages/Levels.js
+++ client/src/pages/Levels.js
@@ -1,20 +1,31 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import BlurBackground from '../components/blurBackground.js';
 function Levels() {
   return (
-    <div className="font-main bg-[#11334E] w-screen h-screen overflow-hidden">
+    <div className="font-main bg-black w-screen h-screen overflow-hidden">
       <UserDisplay name="test" stars="0" />
-      <div className="absolute text-white text-[100px] text-center font-bold w-full h-[150px] backdrop-blur-lg z-30">
-        Levels
+      <div className="absolute left-[200px] items-center w-[calc(100vw-200px)]">
+        <div className="absolute text-white text-[100px] text-center font-bold w-full h-[150px] backdrop-blur z-30">
+          Levels
+        </div>
+        <div className="z-10 top-0 left-0 absolute pt-[150px] h-screen overflow-y-scroll flex flex-col gap-6 items-center w-full">
+          <LevelDisplay name="Div" stars="0" />
+          <LevelDisplay name="Hey" stars="2" />
+          <LevelDisplay name="Did" stars="3" />
+          <LevelDisplay name="Thing" stars="1" />
+        </div>
       </div>
-      <div className="z-0 top-0 left-0 absolute pt-[150px] h-screen overflow-y-scroll flex flex-col gap-6 items-center w-full">
-        <LevelDisplay name="Div" stars="0" />
-        <LevelDisplay name="Hey" stars="2" />
-        <LevelDisplay name="Did" stars="3" />
-        <LevelDisplay name="Thing" stars="1" />
+      <div className="z-0">
+        <BlurBackground glows={
+          [
+            { x: '5%', y: '5%' },
+            { x: '35%', y: '25%' },
+            { x: '95%', y: '50%' }
+          ]
+        } />
       </div>
-    </div>
+    </div >
   );
 }
 
@@ -27,16 +38,20 @@ function UserDisplay({ name, stars }) {
     var test = stars;
   }
   return (
-    <div className="fixed justify-center left-0 backdrop-blur shadow-lg font-bold w-[200px] content-center h-screen flex z-50">
-      <div className="absolute font-main text-black text-[30px] top-0 z-0">
-        {name}
+    <div className="text-white bg-[#E5E7E920] flex flex-col items-center justify-around fixed left-0 backdrop-blur shadow-lg font-bold w-[250px] content-center h-screen z-50">
+      <div className="flex flex-col items-center">
+        <img src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" className="w-[180px] h-[180px] bg-black rounded-full" />
+        <div className="font-main text-[30px] z-0">
+          {name}
+        </div>
       </div>
-      <div className="absolute bg-black rounded-full px-[100px] py-[100px] left-[25px] top-12" />
-      <div className="absolute font-main text-black text-[30px] top-[260px]">
-        Stars
-      </div>
-      <div className="absolute font-main text-black text-[20px] top-[300px]">
-        {test}
+      <div className="flex flex-col items-center">
+        <div className="font-main text-[30px] top-[260px]">
+          Stars
+        </div>
+        <div className="font-main text-[20px] top-[300px]">
+          {test}
+        </div>
       </div>
     </div>
   );
