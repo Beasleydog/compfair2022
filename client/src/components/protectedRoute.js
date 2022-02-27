f92bc66e8c6d360abcafae2eda5e83c301116c38
Beasleydog <beasleypero@gmail.com>
2022-02-26 21:37:10 -0500

    work

---
+++ client/src/components/protectedRoute.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+    // Add your own authentication on the below line.
+    const [loggedIn, setLoginState] = useState(false);
+    return (
+
+        <Route
+            {...rest}
+            render={props =>
+                isLoggedIn ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+                )
+            }
+        />
+    )
+}
+
+export default PrivateRoute
