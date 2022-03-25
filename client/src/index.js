a4c04906673829b4a185d61eb8a44fc59be54ae1
Beasleydog <49841434+Beasleydog@users.noreply.github.com>
2022-03-25 13:10:31 -0400

    test

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import Register from "./pages/Register"
 import Shop from "./pages/Shop"
 import "./styles/output.css"
 
-
+Test
 Display:
   Content of Home if url starts with "/"
   Content of Levels if url starts with "levels"
